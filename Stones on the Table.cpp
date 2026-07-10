c3e7b848f9468d90c6cf310f2f1015f0096095bb
Ahmed Gad <ahmed.said.gad1@gmail.com>
2026-07-10 23:41:30 +0300

    015

---
+++ Stones on the Table.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, counter = 0;
+    string str;
+    cin >> n;
+    cin >> str;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (str[i] == str[i + 1])
+        {
+            counter++;
+        }
+    }
+    cout << counter << endl;
+    return 0;
+}
