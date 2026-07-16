c20a52a11b7cddce448fdcbed165684b4f5d46ff
Ahmed Gad <ahmed.said.gad1@gmail.com>
2026-07-17 02:55:14 +0300

    027

---
+++ Is your horseshoe on the other hoof.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int arr[4];
+    int diff = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + 4);
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (arr[i] != arr[i + 1])
+        {
+            diff++;
+        }
+    }
+
+    cout << 3 - diff << endl;
+
+    return 0;
+}
