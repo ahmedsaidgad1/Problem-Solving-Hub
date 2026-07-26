18263b3a47a9f79819b8535d01f891c2fc871f64
Ahmed Gad <ahmed.said.gad1@gmail.com>
2026-07-26 06:03:16 +0300

    036

---
+++ Sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b == c || a + c == b || c + a == b || c + b == a)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
