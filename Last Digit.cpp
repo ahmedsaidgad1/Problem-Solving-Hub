f2e68afe05ba15fed9fbb5d03ef40e3410710f00
Ahmed Gad <ahmed.said.gad1@gmail.com>
2026-09-14 22:49:06 +0300

    055

---
+++ Last Digit.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long n;
+	cin >> n;
+	if (n % 2 == 0)
+	{
+		cout << 1 << "\n";
+	}
+	else
+	{
+		cout << 6 << "\n";
+	}
+
+	return 0;
+}
