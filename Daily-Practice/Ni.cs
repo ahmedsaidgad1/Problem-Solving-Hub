f5df9805a5bbde5efdf95b358052f33e417f66ca
Ahmed Gad <ahmed.said.gad1@gmail.com>
2026-09-27 00:32:10 +0300

    093

---
+++ Daily-Practice/Ni.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        byte n = Convert.ToByte(Console.ReadLine());
+
+        for (int i=0;i<n;i++)
+        {
+            Console.WriteLine("Ni");
+        }
+    }
+}
